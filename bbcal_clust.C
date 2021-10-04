5a4c570249c3d14427bce5ef4d0ab1bdeb9fa06f
PROVAKAR DATTA <provakardatta@PROVAKARs-MacBook-Pro.local>
2021-10-03 23:01:09 -0400

    Display works. 90% done

--- bbcal_clust.C
+++ bbcal_clust.C
@@ -8,23 +8,12 @@
 #include "fadc_data.h"
 
 const Int_t kNrows = 27;
-const
-Int_t kNcols = 7;
+const Int_t kNcols = 7;
+const Int_t kNrowsPS = 26;
+const Int_t kNcolsPS = 2;
 
-const Int_t kNumModules = kNrows*kNcols;
-const Int_t DISP_MIN_SAMPLE = 85*0;
-const Int_t DISP_MAX_SAMPLE = 135*0+200*0+150*0+50;
-//const Int_t DISP_FADC_SAMPLES = 200;
-const Int_t DISP_FADC_SAMPLES = (DISP_MAX_SAMPLE-DISP_MIN_SAMPLE);
-const Int_t numSamples = 50;
-
-const Int_t minADC = 0;
-const Int_t maxADC = 4000;
 const Int_t kCanvSize = 100;
-
 std::string user_input;
-
-
 Int_t gCurrentEntry = -1;
 
 TChain *T = 0;
@@ -47,14 +36,14 @@ namespace shgui {
   TGNumberEntry *entryInput;
   TGLabel *ledLabel;
 
-  TRootEmbeddedCanvas *canv[4];
+  TRootEmbeddedCanvas *canv[1];
 
   TGCompositeFrame* AddTabSub(Int_t sub) {
     tf = fTab->AddTab(Form("SH Sub%d",sub+1));
 
     TGCompositeFrame *fF5 = new TGCompositeFrame(tf, (12+1)*kCanvSize,(6+1)*kCanvSize , kHorizontalFrame);
     TGLayoutHints *fL4 = new TGLayoutHints(kLHintsTop | kLHintsLeft | kLHintsExpandX |
-        kLHintsExpandY, 5, 5, 5, 5);
+					   kLHintsExpandY, 5, 5, 5, 5);
     TRootEmbeddedCanvas *fEc1 = new TRootEmbeddedCanvas(Form("shSubCanv%d",sub), fF5, 6*kCanvSize,8*kCanvSize);
     //TRootEmbeddedCanvas *fEc1 = new TRootEmbeddedCanvas(0, fF5, 600, 600);
     //Int_t wid = fEc1->GetCanvasWindowId();
@@ -71,14 +60,14 @@ namespace shgui {
     if(!main) {
       main = new TGMainFrame(gClient->GetRoot(), 1000, 900);
       frame1 = new TGHorizontalFrame(main, 150, 20, kFixedWidth);
-      ledLabel = new TGLabel(frame1,"LED Bit:    , Count:      ");
+      ledLabel = new TGLabel(frame1,"Run #: ");
       displayEntryButton = new TGTextButton(frame1,"&Display Entry","clicked_displayEntryButton()");
       entryInput = new TGNumberEntry(frame1,0,5,-1,TGNumberFormat::kNESInteger);
       displayNextButton = new TGTextButton(frame1,"&Next Entry","clicked_displayNextButton()");
       exitButton = new TGTextButton(frame1, "&Exit", 
-          "gApplication->Terminate(0)");
+				    "gApplication->Terminate(0)");
       TGLayoutHints *frame1LH = new TGLayoutHints(kLHintsTop|kLHintsLeft|
-          kLHintsExpandX,2,2,2,2);
+						  kLHintsExpandX,2,2,2,2);
       frame1->AddFrame(ledLabel,frame1LH);
       frame1->AddFrame(displayEntryButton,frame1LH);
       frame1->AddFrame(entryInput,frame1LH);
@@ -96,12 +85,12 @@ namespace shgui {
         tf = AddTabSub(i);
       }
       main->AddFrame(fTab, new TGLayoutHints(kLHintsBottom | kLHintsExpandX |
-                                          kLHintsExpandY, 2, 2, 5, 1));
+					     kLHintsExpandY, 2, 2, 5, 1));
       main->MapSubwindows();
       main->Resize();   // resize to default size
       main->MapWindow();
 
-      for(Int_t i = 0; i < 4; i++) {
+      for(Int_t i = 0; i < 1; i++) {
         subCanv[i] = canv[i]->GetCanvas();
       	// if( kNrows<12 || kNcols<12) {
       	//   //subCanv[i]->Divide(kNrows,kNcols,0.001,0.001);
@@ -109,7 +98,7 @@ namespace shgui {
         // } else {
       	//   subCanv[i]->Divide(12,6,0.001,0.001);
         // }
-      	subCanv[i]->Divide(1,1,1,1);
+      	subCanv[i]->Divide(2,1,0.001,0.001);
       }
     }
   }
@@ -118,20 +107,20 @@ namespace shgui {
 
 Double_t nhit = 0;
 TH1F *histos[kNrows][kNcols];
-Bool_t gSaturated[kNrows][kNcols];
-TH2F* histos_amp = new TH2F("target_amp"," Target FADC Amplitude: PS ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
+TH2F* hSH_amp = new TH2F("sh_amp","Shower ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
+TH2F* hPS_amp = new TH2F("ps_amp","PreShower ; Ncol ; Nrow",kNcolsPS,1,kNcolsPS+1,kNrowsPS,1,kNrowsPS+1);
 
-TH1F* MakeHisto(Int_t row, Int_t col, Int_t bins, const char* suf="")
-{
-  TH1F *h = new TH1F(TString::Format("h%02d%02d%s",row,col, suf),
-      TString::Format("%d-%d",row+1,col+1),bins,DISP_MIN_SAMPLE,DISP_MAX_SAMPLE);
-  h->SetStats(0);
-  h->SetLineWidth(2);
-  h->GetYaxis()->SetLabelSize(0.1);
-  //h->GetYaxis()->SetLabelOffset(-0.17);
-  h->GetYaxis()->SetNdivisions(5);
-  return h;
-}
+// TH1F* MakeHisto(Int_t row, Int_t col, Int_t bins, const char* suf="")
+// {
+//   TH1F *h = new TH1F(TString::Format("h%02d%02d%s",row,col, suf),
+//       TString::Format("%d-%d",row+1,col+1),bins,DISP_MIN_SAMPLE,DISP_MAX_SAMPLE);
+//   h->SetStats(0);
+//   h->SetLineWidth(2);
+//   h->GetYaxis()->SetLabelSize(0.1);
+//   //h->GetYaxis()->SetLabelOffset(-0.17);
+//   h->GetYaxis()->SetNdivisions(5);
+//   return h;
+// }
 
 
 bool is_number(const std::string& mystring)
@@ -141,7 +130,7 @@ bool is_number(const std::string& mystring)
   return !mystring.empty() && it == mystring.end();
 }
 
-void displayEvent(Int_t entry = -1)
+void displayEvent(Int_t entry = -1, Int_t run = 7 )
 {
   if(entry == -1) {
     gCurrentEntry++;
@@ -155,26 +144,26 @@ void displayEvent(Int_t entry = -1)
 
   T->GetEntry(gCurrentEntry);
   std::cout << "Displaying event " << gCurrentEntry << std::endl;
-  shgui::ledLabel->SetText(TString::Format("LED Bit: %02d, Count: %5d",Int_t(fadc_datat::ledbit),Int_t(fadc_datat::ledcount)));
+  shgui::ledLabel->SetText(TString::Format("Run #: %d",run));
 
   Int_t r,c,idx,n,sub;
   // Clear old histograms, just in case modules are not in the tree
-  for(r = 0; r < kNrows; r++) {
-    for(c = 0; c < kNcols; c++) {
-      histos[r][c]->Reset("ICES M");
-      gSaturated[r][c] = false;
-    }
-  }
+  hSH_amp->Reset("ICES M");
+  hPS_amp->Reset("ICES M");
 
-  Float_t amp[kNrows][kNcols];
+  Float_t amp[kNrows][kNcols], ampPS[kNrowsPS][kNcolsPS];
   Float_t peak[kNrows][kNcols];
-  Double_t adc[kNrows][kNcols];
-  Double_t tdc[kNrows][kNcols];
+  Double_t adc[kNrows][kNcols], adcPS[kNrowsPS][kNcolsPS];
+  Double_t tdc[kNrows][kNcols], tdcPS[kNrowsPS][kNcolsPS];
   for(r  = 0; r < kNrows; r++) {
     for(c  = 0; c < kNcols; c++) {
       peak[r][c] = 0.0;
       adc[r][c] = 0.0;
+      amp[r][c] = 0.0;
       tdc[r][c] = 0.0;
+      adcPS[r][c] = 0.0;
+      ampPS[r][c] = 0.0;
+      tdcPS[r][c] = 0.0;
     }
   }
   for(Int_t m = 0; m < fadc_datat::ndata; m++) {
@@ -191,68 +180,52 @@ void displayEvent(Int_t entry = -1)
     adc[r][c] = fadc_datat::a[m];
     tdc[r][c] = fadc_datat::tdc[m];
     amp[r][c] = fadc_datat::amp[m];
-    //std::cout << "n=" << fadc_datat::nsamps[m] << std::endl;
-    //bool displayed = false;
-    // for(Int_t s = DISP_MIN_SAMPLE; s < DISP_MAX_SAMPLE && s < n; s++) {
-    //   displayed = true;
-    //   histos[r][c]->SetBinContent(s+1-DISP_MIN_SAMPLE,fadc_datat::samps[idx+s]);
-    //   //histos_amp[r][c]->SetBinContent(int(tdc[r][c]/4.0)+1,fadc_datat::amp[m]);
-    //   histos_amp[r][c]->Fill( float(c+1),float(r+1),fadc_datat::amp[m] );
-    //   if(peak[r][c]<fadc_datat::samps[idx+s])
-    //     peak[r][c]=fadc_datat::samps[idx+s];
-    //   if(peak[r][c]>4095) {
-    //     gSaturated[r][c] = true;
-    //   }
-    //   //std::cout << "setting bin content: [" << r+1 << ", " << c+1 << ", " << s << "] = " << fadc_datat::samps[idx+s] << std::endl;
-    // }
 
-    //if( tdc[r][c]>0 ){
-    //displayed = true;
-      histos_amp->Fill( double(c+1),double(r+1),fadc_datat::amp[m] );
-      //}
+    if( tdc[r][c]>0 ){
+      //displayed = true;
+      hSH_amp->Fill( double(c+1),double(r+1),fadc_datat::amp[m] );
+    }
 
-    
-    // if(!displayed) {
-    //   std::cerr << "Skipping empty module: " << m << std::endl;
-    //   for(Int_t s = 0;  s < DISP_FADC_SAMPLES; s++) {
-    //     histos[r][c]->SetBinContent(s+1,-404);
-    //   }
-    // }
   }
 
-  subCanv[0]->cd();
-  // for(r = 0; r < kNrows; r++) {
-  //   for(c = 0; c < kNcols; c++) {
-      // sub = r/7;
-      // //subCanv[sub]->cd(c*kNrows + r + 1);
-      // subCanv[sub]->cd((r%7)*kNcols + c + 1);
-      // histos[r][c]->SetTitle(TString::Format("%d-%d (ADC=%g,TDC=%g)",r+1,c+1,adc[r][c],tdc[r][c]));
-      // if(gSaturated[r][c])
-      //   histos[r][c]->SetLineColor(kRed+1);
-      // else
-      //   histos[r][c]->SetLineColor(kBlue+1);
-      // if(tdc[r][c]!=0)
-      //   histos[r][c]->SetLineColor(kGreen+1);
-      // TLine* L = new TLine(tdc[r][c]/4.0, 0, tdc[r][c]/4.0, peak[r][c]);
-      // L->SetLineColor(kMagenta+1);
-      // L->SetLineWidth(2);
-      // histos_amp[r][c]->SetLineColor(kBlack);
-      
-      // if(histos_amp[r][c]->GetMaximum()>histos[r][c]->GetMaximum())
-      // 	histos[r][c]->SetMaximum(histos_amp[r][c]->GetMaximum()*1.1);
-      // histos[r][c]->Draw();
-      histos_amp->Draw("text colz");
-      // if(tdc[r][c]!=0)L->Draw("same");
-      // gPad->Update();
-      // std::cout << " [" << r << ", " << c << "]=" << peak[r][c];
-  //   }
-  // }
-  std::cout << std::endl;
-  //gSystem->mkdir("images/",kTRUE);
-  //std::cerr << "Saving canvas!" << std::endl;
-  //canvas->SaveAs("images/display_sh.png");
-  //  canvVector[i]->SaveAs(TString::Format("images/canvas_%d.png",int(i)));
-
+  for(Int_t m = 0; m < fadc_datat::ndataPS; m++) {
+    r = fadc_datat::rowPS[m];//-1;
+    c = fadc_datat::colPS[m];//-1;
+    if(r < 0 || c < 0) {
+      std::cerr << "Why is row negative? Or col?" << std::endl;
+      continue;
+    }
+    if(r>= kNrowsPS || c >= kNcolsPS)
+      continue;
+    adcPS[r][c] = fadc_datat::aPS[m];
+    tdcPS[r][c] = fadc_datat::tdcPS[m];
+    ampPS[r][c] = fadc_datat::ampPS[m];
+    
+    if( tdcPS[r][c]>0 ){
+      hPS_amp->Fill( double(c),double(r+1),fadc_datat::ampPS[m] );
+    }
+  }
+    
+  subCanv[0]->cd(1);
+  gPad->SetGridx();
+  gPad->SetGridy();
+  hSH_amp->SetStats(0);
+  hSH_amp->SetMaximum(50);
+  hSH_amp->SetMinimum(0); 
+  hSH_amp->GetYaxis()->SetNdivisions(kNrows);
+  hSH_amp->GetXaxis()->SetNdivisions(kNcols);
+  hSH_amp->Draw("text colz");
+  gPad->Update();
+  subCanv[0]->cd(2);
+  gPad->SetGridx();
+  gPad->SetGridy();
+  hPS_amp->SetStats(0);
+  hPS_amp->SetMaximum(50);
+  hPS_amp->SetMinimum(0); 
+  hPS_amp->GetYaxis()->SetNdivisions(kNrowsPS);
+  hPS_amp->GetXaxis()->SetNdivisions(kNcolsPS);
+  hPS_amp->Draw("text colz");
+  gPad->Update();
 }
 
 void clicked_displayNextButton()
@@ -274,32 +247,11 @@ Int_t display(Int_t run = 290, Int_t event = -1)
   shgui::SetupGUI();
   gStyle->SetLabelSize(0.05,"XY");
   gStyle->SetTitleFontSize(0.08);
-
-  // if(!T) { 
-  //   T = new TChain("T");
-  //   TString dataDIR = gSystem->Getenv("OUT_DIR");
-  //   TString filename = dataDIR + "/bbshower_"+run+"_500000.root";
-  //   cout << filename << endl;
-  //   // T->Add(TString::Format("/home/bbshower/sbs_devel/ReplayedFiles/bbshower_%d_10000.root",run));
-  //   T->SetBranchStatus("*",0);
-  //   T->SetBranchStatus("bb.sh.*",1);
-  //   T->SetBranchAddress("bb.sh.nsamps",fadc_datat::nsamps);
-  //   T->SetBranchAddress("bb.sh.a",fadc_datat::a);
-  //   T->SetBranchAddress("bb.sh.a_time",fadc_datat::tdc);
-  //   T->SetBranchAddress("bb.sh.a_amp_p",fadc_datat::amp);
-  //   //T->SetBranchAddress("bb.sh.ledbit",&fadc_datat::ledbit);
-  //   //T->SetBranchAddress("bb.sh.ledcount",&fadc_datat::ledcount);
-  //   T->SetBranchAddress("bb.sh.samps",fadc_datat::samps);
-  //   T->SetBranchAddress("bb.sh.samps_idx",fadc_datat::samps_idx);
-  //   T->SetBranchAddress("bb.sh.adcrow",fadc_datat::row);
-  //   T->SetBranchAddress("bb.sh.adccol",fadc_datat::col);
-  //   T->SetBranchStatus("Ndata.bb.sh.adcrow",1);
-  //   T->SetBranchAddress("Ndata.bb.sh.adcrow",&fadc_datat::ndata);
-  //   std::cerr << "Opened up tree with nentries=" << T->GetEntries() << std::endl;
   if(!T) { 
     T = new TChain("T");
-    TString dataDIR = gSystem->Getenv("OUT_DIR");
-    TString filename = dataDIR + "/bbshower_"+run+"_500000.root";
+    //TString dataDIR = gSystem->Getenv("OUT_DIR");
+    //TString filename = dataDIR + "/bbshower_"+run+"_500000.root";
+    TString filename = "../bbshower_434_30000.root";
     event = -1;
     T->Add(filename);
     T->SetBranchStatus("*",0);
@@ -311,19 +263,15 @@ Int_t display(Int_t run = 290, Int_t event = -1)
     T->SetBranchAddress("bb.sh.adccol",fadc_datat::col);
     T->SetBranchStatus("Ndata.bb.sh.adcrow",1);
     T->SetBranchAddress("Ndata.bb.sh.adcrow",&fadc_datat::ndata);
-    // T->SetBranchAddress("bb.sh.nsamps",fadc_datat::nsamps);
-    // T->SetBranchAddress("bb.sh.samps",fadc_datat::samps);
-    // T->SetBranchAddress("bb.sh.samps_idx",fadc_datat::samps_idx);    
+    T->SetBranchStatus("bb.ps.*",1);
+    T->SetBranchAddress("bb.ps.a_p",fadc_datat::aPS);
+    T->SetBranchAddress("bb.ps.a_amp_p",fadc_datat::ampPS);
+    T->SetBranchAddress("bb.ps.a_time",fadc_datat::tdcPS);
+    T->SetBranchAddress("bb.ps.adcrow",fadc_datat::rowPS);
+    T->SetBranchAddress("bb.ps.adccol",fadc_datat::colPS);
+    T->SetBranchStatus("Ndata.bb.ps.adcrow",1);
+    T->SetBranchAddress("Ndata.bb.ps.adcrow",&fadc_datat::ndataPS);
     cout << "Opened up tree with nentries=" << T->GetEntries() << endl;
-    for(Int_t r = 0; r < kNrows; r++) {
-      for(Int_t c = 0; c < kNcols; c++) {
-        histos[r][c] = MakeHisto(r,c,DISP_FADC_SAMPLES);
-        //histos_amp[r][c] = MakeHisto(r,c,DISP_FADC_SAMPLES,"_amp");
-        gSaturated[r][c] = false;
-      }
-    }
-    //canvas = new TCanvas("canvas","canvas",kCanvSize*kNcols,kCanvSize*kNrows);
-    //canvas->Divide(kNcols,kNrows);
   }
   return 0;
   gCurrentEntry = event;
@@ -334,7 +282,7 @@ Int_t display(Int_t run = 290, Int_t event = -1)
       gCurrentEntry++;
     }
     //dis1(event);
-    displayEvent(gCurrentEntry);
+    displayEvent(gCurrentEntry,run);
     std::cout << "Display options: <enter> == next event, or q to stop." << std::endl;
     //std::cin >> user_input;
     getline(std::cin,user_input);
