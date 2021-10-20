c9bb90a7a5c9139e088a4639e90d3b83e6498924
Hall A Online <a-onl@jlab.org>
2021-10-19 22:59:04 -0400

    Updated with tracking info

--- bbcal_clustD.C
+++ bbcal_clustD.C
@@ -13,6 +13,9 @@ const Int_t kNcols = 7;
 const Int_t kNrowsPS = 26;
 const Int_t kNcolsPS = 2;
 
+const Double_t zposSH = 1.901952; //m
+const Double_t zposPS = 1.695704; //m
+
 const Int_t kCanvSize = 100;
 std::string user_input;
 Int_t gCurrentEntry = -1;
@@ -82,7 +85,7 @@ namespace shgui {
       fL3 = new TGLayoutHints(kLHintsTop | kLHintsLeft, 5, 5, 5, 5);
 
       // Create Tab1 (SH Sub1)
-      for(Int_t i = 0; i < 4; i++) {
+      for(Int_t i = 0; i < 5; i++) {
         tf = AddTabSub(i);
       }
       main->AddFrame(fTab, new TGLayoutHints(kLHintsBottom | kLHintsExpandX |
@@ -91,7 +94,7 @@ namespace shgui {
       main->Resize();   // resize to default size
       main->MapWindow();
 
-      for(Int_t i = 0; i < 4; i++) {
+      for(Int_t i = 0; i < 5; i++) {
         subCanv[i] = canv[i]->GetCanvas();
       	// if( kNrows<12 || kNcols<12) {
       	//   //subCanv[i]->Divide(kNrows,kNcols,0.001,0.001);
@@ -109,10 +112,12 @@ Double_t nhit = 0;
 TH1F *histos[kNrows][kNcols];
 TH2F* hSH_int = new TH2F("sh_int","Shower(a_p) ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
 TH2F* hSH_intEng = new TH2F("sh_intE","Shower(a_c) ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
-TH2F* hSH_clus_e = new TH2F("sh_clus_e","Shower(Cluster) ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
+TH2F* hSH_clus_e = new TH2F("sh_clus_e","Shower Cluster ; Ncol ; Nrow",kNcols,1,kNcols+1,kNrows,1,kNrows+1);
+TH2F* hSH_xypos = new TH2F("sh_cl_tr","SH Clust. + Track Pos. ; y ; x",kNcols,-0.2992,0.2992,kNrows,-1.1542,1.1542);
 TH2F* hPS_int = new TH2F("ps_int","PreShower(a_p) ; Ncol ; Nrow",kNcolsPS,1,kNcolsPS+1,kNrowsPS,1,kNrowsPS+1);
 TH2F* hPS_intEng = new TH2F("ps_intE","PreShower(a_c) ; Ncol ; Nrow",kNcolsPS,1,kNcolsPS+1,kNrowsPS,1,kNrowsPS+1);
-TH2F* hPS_clus_e = new TH2F("ps_clus_e","PreShower(Cluster) ; Ncol ; Nrow",kNcolsPS,1,kNcolsPS+1,kNrowsPS,1,kNrowsPS+1);
+TH2F* hPS_clus_e = new TH2F("ps_clus_e","PreShower Cluster ; Ncol ; Nrow",kNcolsPS,1,kNcolsPS+1,kNrowsPS,1,kNrowsPS+1);
+TH2F* hPS_xypos = new TH2F("ps_cl_tr","PS Clust. + Track Pos.; y ; x",kNcolsPS,-0.3705,0.3705,kNrowsPS,-1.201,1.151);
 
 bool is_number(const std::string& mystring)
 {
@@ -138,8 +143,6 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
   std::cout << "Displaying event " << gCurrentEntry << std::endl;
   shgui::ledLabel->SetText(TString::Format("Run #: %d",run));
 
-  Int_t r,c;
-
   // Clear old histograms, just in case modules are not in the tree
   hSH_int->Reset("ICES M");
   hSH_intEng->Reset("ICES M");
@@ -147,44 +150,63 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
   hPS_intEng->Reset("ICES M");
   //Clustering
   hSH_clus_e->Reset("ICES M");
+  hSH_xypos->Reset("ICES M");
   hPS_clus_e->Reset("ICES M");
+  hPS_xypos->Reset("ICES M");
 
   // Shower
   for(Int_t m = 0; m < T->Ndata_bb_sh_adcrow; m++) {
-    r = T->bb_sh_adcrow[m];
-    c = T->bb_sh_adccol[m];
+    int r = T->bb_sh_adcrow[m];
+    int c = T->bb_sh_adccol[m];
     if(r < 0 || c < 0) {
       std::cerr << "Why is row negative? Or col?" << std::endl;
       continue;
     }
     if(r>= kNrows || c >= kNcols)
       continue;
-
     if( T->bb_sh_a_time[m]>0 ){
-     hSH_int->Fill( double(c+1),double(r+1), T->bb_sh_a_p[m] );
-     hSH_intEng->Fill( double(c+1),double(r+1), T->bb_sh_a_c[m] );     
-    } 
+     hSH_int->Fill( double(c+1), double(r+1), T->bb_sh_a_p[m] );
+     cout << "r " << r << " c= " << c <<  "a_c= " << T->bb_sh_a_c[m] << endl;								     
+     hSH_intEng->Fill( double(c+1), double(r+1), T->bb_sh_a_c[m] );     
+    }
   }
+
+  // for(int ihit=0;ihit<T->Ndata_bb_sh_a_p;ihit++){
+  //   int r = T->bb_sh_adcrow[ihit];
+  //   int c = T->bb_sh_adcrow[ihit];
+  //   if(r < 0 || c < 0) {
+  //     std::cerr << "Why is row negative? Or col?" << std::endl;
+  //     continue;
+  //   }
+  //   if(r>= kNrows || c >= kNcols)
+  //     continue;
+  //   hSH_int->Fill( double(c+1),double(r+1), T->bb_sh_a_p[ihit] );
+  //   hSH_intEng->Fill( double(c+1),double(r+1), T->bb_sh_a_c[ihit] ); 
+
+  //   cout << " ***Ndata= " << T->Ndata_bb_sh_a_p << " a_c= " << T->bb_sh_a_c[ihit] << " a_p= " <<  T->bb_sh_a_p[ihit] << endl;
+  // }
   
   // SH Clustering
   cout << "Number of SH clusters in this event= " << T->bb_sh_nclus << endl;
   for( int cl=0; cl<(int)T->bb_sh_nclus; cl++ ){
-    cout << "Clus ID " << T->bb_sh_clus_id[cl] << "\t" << "Clus e_c= " << T->bb_sh_clus_e_c[cl];
+    cout << "Clus ID " << T->bb_sh_clus_id[cl] << "\t" << "Clus e= " << T->bb_sh_clus_e[cl];
     cout << "\tNo. of blocks involved= " << T->bb_sh_clus_nblk[cl] << endl;
     int cID = (int)T->bb_sh_clus_id[cl];
     if( cID!=-1 ){
-      int rCl = (int)T->bb_sh_adcrow[cID];
-      int cCl = (int)T->bb_sh_adccol[cID];
-      hSH_clus_e->Fill( double(cCl+1), double(rCl+1), T->bb_sh_clus_e_c[cl] );
+      int rCl = (int)T->bb_sh_clus_row[cl];
+      int cCl = (int)T->bb_sh_clus_col[cl];
+      hSH_clus_e->Fill( double(cCl+1), double(rCl+1), T->bb_sh_clus_e[cl] );
       if( cl==0 ){
 	for( int b = 0; b < (int)T->bb_sh_clus_nblk[cl]; b++ ){
 	  int cblkID = (int)T->bb_sh_clus_blk_id[b];
-	  int rblkCl = (int)T->bb_sh_adcrow[cblkID];
-	  int cblkCl = (int)T->bb_sh_adccol[cblkID];
+	  int rblkCl = (int)T->bb_sh_clus_blk_row[b];
+	  int cblkCl = (int)T->bb_sh_clus_blk_col[b];
+	  double xblkCl = T->bb_sh_clus_blk_x[b];
+	  double yblkCl = T->bb_sh_clus_blk_y[b];
 	  if(cblkID!=cID){
-	    cout << "Clus Blk ID " << T->bb_sh_clus_blk_id[b] << "\t" << "Clus Blk e_c= " << T->bb_sh_a_c[cblkID] << endl; 
-	    hSH_clus_e->Fill( double(cblkCl+1), double(rblkCl+1), T->bb_sh_a_c[cblkID] );
+	    hSH_clus_e->Fill( double(cblkCl+1), double(rblkCl+1), T->bb_sh_clus_blk_e[b] );
 	  }
+	  hSH_xypos->Fill( yblkCl, xblkCl, T->bb_sh_clus_blk_e[b] );
 	}
       }
     }
@@ -194,21 +216,33 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
   
   // Pre-Shower
   for(Int_t m = 0; m < T->Ndata_bb_ps_adcrow; m++) {
-    r = T->bb_ps_adcrow[m];
-    c = T->bb_ps_adccol[m];
+    int r = T->bb_ps_adcrow[m];
+    int c = T->bb_ps_adccol[m];
     if(r < 0 || c < 0) {
       std::cerr << "Why is row negative? Or col?" << std::endl;
       continue;
     }
     if(r>= kNrows || c >= kNcols)
       continue;
-
     if( T->bb_ps_a_time[m]>0 ){
-     hPS_int->Fill( double(c),double(r+1), T->bb_ps_a_p[m] ); 
-     hPS_intEng->Fill( double(c),double(r+1), T->bb_ps_a_c[m] );     
+      hPS_int->Fill( double(c+1),double(r+1), T->bb_ps_a_p[m] ); 
+      hPS_intEng->Fill( double(c+1),double(r+1), T->bb_ps_a_c[m] );     
     }
   }
 
+  // for(int ihit=0;ihit<T->Ndata_bb_ps_a_p;ihit++){
+  //   int r = T->bb_ps_adcrow[ihit];
+  //   int c = T->bb_ps_adcrow[ihit];
+  //   if(r < 0 || c < 0) {
+  //     std::cerr << "Why is row negative? Or col?" << std::endl;
+  //     continue;
+  //   }
+  //   if(r>= kNrows || c >= kNcols)
+  //     continue;
+  //   hPS_int->Fill( double(c+1),double(r+1), T->bb_ps_a_p[ihit] );
+  //   hPS_intEng->Fill( double(c+1),double(r+1), T->bb_ps_a_c[ihit] );     
+  // }
+  
   // PS Clustering
   cout << "Number of PS clusters in this event= " << T->bb_ps_nclus << endl;
   for( int cl=0; cl<(int)T->bb_ps_nclus; cl++ ){
@@ -216,43 +250,71 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
     cout << "\tNo. of blocks involved= " << T->bb_ps_clus_nblk[cl] << endl;
     int cID = (int)T->bb_ps_clus_id[cl];
     if( cID!=-1 ){
-      int rCl = (int)T->bb_ps_adcrow[cID];
-      int cCl = (int)T->bb_ps_adccol[cID];
-      hPS_clus_e->Fill( double(cCl+1), double(rCl+1), T->bb_ps_clus_e_c[cl] );
+      int rCl = (int)T->bb_ps_clus_row[cl];
+      int cCl = (int)T->bb_ps_clus_col[cl];
+      hPS_clus_e->Fill( double(cCl+1), double(rCl+1), T->bb_ps_clus_e[cl] );
       if( cl==0 ){
 	for( int b = 0; b < (int)T->bb_ps_clus_nblk[cl]; b++ ){
 	  int cblkID = (int)T->bb_ps_clus_blk_id[b];
-	  int rblkCl = (int)T->bb_ps_adcrow[cblkID];
-	  int cblkCl = (int)T->bb_ps_adccol[cblkID];
+	  int rblkCl = (int)T->bb_ps_clus_blk_row[b];
+	  int cblkCl = (int)T->bb_ps_clus_blk_col[b];
+	  double xblkCl = T->bb_ps_clus_blk_x[b];
+	  double yblkCl = T->bb_ps_clus_blk_y[b];
 	  if(cblkID!=cID){
-	    cout << "Clus Blk ID " << T->bb_ps_clus_blk_id[b] << "\t" << "Clus Blk e_c= " << T->bb_ps_a_c[cblkID] << endl; 
-	    hPS_clus_e->Fill( double(cblkCl+1), double(rblkCl+1), T->bb_ps_a_c[cblkID] );
+	    hPS_clus_e->Fill( double(cblkCl+1), double(rblkCl+1), T->bb_ps_clus_blk_e[b] );
 	  }
+	  hPS_xypos->Fill( yblkCl, xblkCl, T->bb_ps_clus_blk_e[b] );
 	}
       }
     }
   }
   
+  // Tracking stuff (Drawing circle)
+  // double xTrATsh = T->BB_gold_x + zposSH*T->BB_gold_th;
+  // double yTrATsh = T->BB_gold_y + zposSH*T->BB_gold_ph;
+  double xTrATsh = T->bb_tr_x[0] + zposSH*T->bb_tr_th[0];
+  double yTrATsh = T->bb_tr_y[0] + zposSH*T->bb_tr_ph[0];
+  TEllipse *trackPOSatSH = new TEllipse(yTrATsh,xTrATsh,.05,.07);
+  trackPOSatSH->SetFillStyle(0);
+  trackPOSatSH->SetLineWidth(2);
+  trackPOSatSH->SetLineColor(kRed);
+  TEllipse *dotSH = new TEllipse(yTrATsh,xTrATsh,.008,.008);
+  dotSH->SetFillStyle(1001);
+  dotSH->SetFillColor(1);
+
+  double xTrATps = T->bb_tr_x[0] + zposPS*T->bb_tr_th[0];
+  double yTrATps = T->bb_tr_y[0] + zposPS*T->bb_tr_ph[0];
+  TEllipse *trackPOSatPS = new TEllipse(yTrATps,xTrATps,.05,.07);
+  trackPOSatPS->SetFillStyle(0);
+  trackPOSatPS->SetLineWidth(2);
+  trackPOSatPS->SetLineColor(kRed);
+  TEllipse *dotPS = new TEllipse(yTrATps,xTrATps,.008,.008);
+  dotPS->SetFillStyle(1001);
+  dotPS->SetFillColor(1);
   
   subCanv[0]->cd(1);
   gPad->SetGridx();
   gPad->SetGridy();
-  hSH_intEng->SetStats(0);
-  hSH_intEng->SetMaximum(0.5);
-  hSH_intEng->SetMinimum(0); 
-  hSH_intEng->GetYaxis()->SetNdivisions(kNrows);
-  hSH_intEng->GetXaxis()->SetNdivisions(kNcols);
-  hSH_intEng->Draw("text colz");
+  hSH_xypos->SetStats(0);
+  hSH_xypos->SetMaximum(0.5);
+  hSH_xypos->SetMinimum(0); 
+  hSH_xypos->GetYaxis()->SetNdivisions(kNrows);
+  hSH_xypos->GetXaxis()->SetNdivisions(kNcols);
+  hSH_xypos->Draw("text colz");
+  dotSH->Draw("same");
+  trackPOSatSH->Draw("same");
   gPad->Update();
   subCanv[0]->cd(2);
   gPad->SetGridx();
   gPad->SetGridy();
-  hPS_intEng->SetStats(0);
-  hPS_intEng->SetMaximum(0.5);
-  hPS_intEng->SetMinimum(0); 
-  hPS_intEng->GetYaxis()->SetNdivisions(kNrowsPS);
-  hPS_intEng->GetXaxis()->SetNdivisions(kNcolsPS);
-  hPS_intEng->Draw("text colz");
+  hPS_xypos->SetStats(0);
+  hPS_xypos->SetMaximum(0.5);
+  hPS_xypos->SetMinimum(0); 
+  hPS_xypos->GetYaxis()->SetNdivisions(kNrowsPS);
+  hPS_xypos->GetXaxis()->SetNdivisions(kNcolsPS);
+  hPS_xypos->Draw("text colz");
+  dotPS->Draw("same");
+  trackPOSatPS->Draw("same");
   gPad->Update();
 
   subCanv[1]->cd(1);
@@ -306,7 +368,6 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
   hPS_intEng->GetYaxis()->SetNdivisions(kNrowsPS);
   hPS_intEng->GetXaxis()->SetNdivisions(kNcolsPS);
   hPS_intEng->Draw("text colz");
-  gPad->Update();
   subCanv[3]->cd(2);
   gPad->SetGridx();
   gPad->SetGridy();
@@ -316,6 +377,27 @@ void displayEvent(Int_t entry = -1, Int_t run = 7 )
   hPS_clus_e->GetYaxis()->SetNdivisions(kNrowsPS);
   hPS_clus_e->GetXaxis()->SetNdivisions(kNcolsPS);
   hPS_clus_e->Draw("text colz");
+  gPad->Update();
+
+  subCanv[4]->cd(1);
+  gPad->SetGridx();
+  gPad->SetGridy();
+  hSH_intEng->SetStats(0);
+  hSH_intEng->SetMaximum(0.5);
+  hSH_intEng->SetMinimum(0); 
+  hSH_intEng->GetYaxis()->SetNdivisions(kNrows);
+  hSH_intEng->GetXaxis()->SetNdivisions(kNcols);
+  hSH_intEng->Draw("text colz");
+  gPad->Update();
+  subCanv[4]->cd(2);
+  gPad->SetGridx();
+  gPad->SetGridy();
+  hPS_intEng->SetStats(0);
+  hPS_intEng->SetMaximum(0.5);
+  hPS_intEng->SetMinimum(0); 
+  hPS_intEng->GetYaxis()->SetNdivisions(kNrowsPS);
+  hPS_intEng->GetXaxis()->SetNdivisions(kNcolsPS);
+  hPS_intEng->Draw("text colz");
   gPad->Update();
 }
 
@@ -339,8 +421,8 @@ Int_t display(Int_t run = 290, Int_t event = 50000)
   gStyle->SetLabelSize(0.05,"XY");
   gStyle->SetTitleFontSize(0.08);
 
-  TString filename = "../bbshower_434_5000.root";
-  //TString filename = Form("$OUT_DIR/bbshower_%d_%d.root",run,event);
+  //TString filename = "bbshower_434_5000.root";
+  TString filename = Form("$OUT_DIR/mkj_bbshower_%d_%d.root",run,event);
   TFile *f = TFile::Open(filename); 
   TChain *C = (TChain*)f->Get("T");
   cout << "Opened up tree with nentries=" << C->GetEntries() << endl;
